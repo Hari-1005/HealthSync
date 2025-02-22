cc3ecdd6ac1aa9b928c970c2f53c1cc68b24c522
hariteja <hariteja.in@gmail.com>
2025-02-22 21:46:22 +0530

    admin controller and authorization

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,7 +1,8 @@
 import validator from "validator";
 import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
-import fs from 'fs'
+import fs from "fs";
+import jwt from "jsonwebtoken";
 import doctorModel from "../models/doctorModel.js";
 
 //--Add Doctor---//
@@ -38,7 +39,7 @@ export const addDoctor = async (req, res) => {
     }
 
     if (!imageFile) {
-        return res.json({ success: false, message: "No image uploaded" });
+      return res.json({ success: false, message: "No image uploaded" });
     }
 
     //validating email format
@@ -68,13 +69,13 @@ export const addDoctor = async (req, res) => {
     const imageUrl = imageUpload.secure_url;
 
     // Delete the uploaded file from local storage
-fs.unlink(imageFile.path, (err) => {
-    if (err) {
-      console.error("Error deleting file:", err);
-    } else {
-      console.log("Temp file deleted:", imageFile.path);
-    }
-})
+    fs.unlink(imageFile.path, (err) => {
+      if (err) {
+        console.error("Error deleting file:", err);
+      } else {
+        console.log("Temp file deleted:", imageFile.path);
+      }
+    });
 
     const doctorData = {
       name,
@@ -88,13 +89,32 @@ fs.unlink(imageFile.path, (err) => {
       available,
       fees,
       address: JSON.parse(address),
-      date : Date.now()
+      date: Date.now(),
     };
 
     const newDoctor = new doctorModel(doctorData);
     await newDoctor.save();
 
     res.json({ success: true, message: "Doctor added" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//--Admin Login----//
+export const adminLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (
+      email === process.env.ADMIN_EMAIL &&
+      password === process.env.ADMIN_PASSWORD
+    ) {
+      const token = jwt.sign(email + password, process.env.JWT_SECRET_CODE);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid Credentials" });
+    }
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
